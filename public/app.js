094db685f8972dc33fa2b13a9e094a385450d917
rduquesnoy <rduquesnoy@drwholdings.com>
2015-08-19 14:24:09 -0500

    getURL added

--- public/app.js
+++ public/app.js
@@ -14,7 +14,6 @@ function googleSignIn(googleUser) {
     var params = {
       Bucket: 'downloads.epitopes.net' /* required */
     };
-    console.log(arguments)
     var s3 = new AWS.S3();
     s3.listObjects(params, function(err, data) {
       if (err) console.log(err, err.stack); // an error occurred
