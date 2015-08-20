309b381b3582ec66d0860b6d3b3468b14cf962b1
rduquesnoy <rduquesnoy@drwholdings.com>
2015-08-20 09:56:51 -0500

    auto update url for downloads

--- public/app.js
+++ public/app.js
@@ -17,7 +17,10 @@ function googleSignIn(googleUser) {
     var s3 = new AWS.S3();
     s3.listObjects(params, function(err, data) {
       if (err) console.log(err, err.stack); // an error occurred
-        else     console.log(data);           // successful response
+    });
+    $(".downloads_url").each( function (index, value) {
+      var url = getURL($(value).attr('id') + ".xlsb"); 
+      $(value).attr('href', url); 
     });
   });
 }
